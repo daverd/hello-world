40d08ecd990a5a5bc6b736c98c90f5d018deaa7f
daverd <davidrd.13@gmail.com>
2015-02-23 17:29:59 +0100

    add matlab files
    blabalblab

---
+++ Sorting_function.m
@@ -0,0 +1,22 @@
+function Sorting_function (firstArray)
+% SORTING_FUNCTION sorts arrays
+%
+% SORTING_FUNCTION (array)
+%
+% This function sorts arrays using the bubble sort method
+% blalbalblalbablalbalbalbalbalbalbablablab
+
+%% Algorithm
+    for i = 2 : length(firstArray) + 1
+       for j = 1 : length(firstArray) + (1 - i)
+           if (firstArray(j) > firstArray(j+1))
+              auxiliaryVariable = firstArray(j);
+              firstArray(j)     = firstArray(j+1);
+              firstArray(j+1)   = auxiliaryVariable;
+           end %if
+       end % for j
+    end %for i
+    
+%% Display the results
+    firstArray  
+end 
